e69ae5a70373f6fbaec1c50abbc7e6f3ab62effe
durga-d <durgadinakaran01@gmail.com>
2022-03-17 20:03:39 +0530

    Patter Programs added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,33 @@
 package com.company;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
-        System.out.println("Hello!");
+    public static void main(String[] args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1 || j==n || i==1 || i==n){
+                    System.out.print("$ ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        int alphabet=64;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)(j+alphabet)+" ");
+            }
+            System.out.println();
+        }
     }
 }
+
+

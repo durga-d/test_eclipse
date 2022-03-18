1d8f0df53e04e59110f35a895fdbe17dac30f413
durga-d <durgadinakaran01@gmail.com>
2022-03-18 19:31:25 +0530

    Added new comments in Inheritance Demo

--- src/com/company/InheritanceDemo.java
+++ src/com/company/InheritanceDemo.java
@@ -6,15 +6,16 @@ public class InheritanceDemo {
         c.show(2,3);
         CalcAdv ca=new CalcAdv();
         ca.show(2,3);
+        ca.display();
         CalcAdvPro cap=new CalcAdvPro();
         cap.show(2,3);
 
 //        CalcAdvPro cap1=new Calculator();   not possible
-        Calculator cap1=new CalcAdvPro();
-        cap.show(2,3);
-
-        CalcAdv ca1=new CalcAdvPro();
+        Calculator ca1=new CalcAdv();  //Calculator is just a ref here. But CalcAdvPro is the implementation
         ca1.show(2,3);
+//        ca1.display(); //variable ca1 is of type Calculator which doesn't have access to display() method. But show() is an overridden method, so that alone can be accessed.
+        Calculator cap1=new CalcAdvPro(); // bcoz CalcAdvPro holds an overridden method from Calculator. show()
+        cap1.show(2,3);
     }
 }
 class Calculator{
@@ -31,6 +32,9 @@ class CalcAdv extends Calculator{
     }
     void show(int i,int j){
         System.out.println("CalcAdv : "+add(i,j)+" "+sub(i,j));
+    }
+    void display(){
+        System.out.println("A display mesage");
     }
 }
 class CalcAdvPro extends CalcAdv{

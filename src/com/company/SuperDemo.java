3d3f295c4d27e363a0c97a1fe7de8b34603f18a9
durga-d <durgadinakaran01@gmail.com>
2022-03-18 18:04:40 +0530

    Updated SuperDemo with uses of super in var and methods

--- src/com/company/SuperDemo.java
+++ src/com/company/SuperDemo.java
@@ -2,16 +2,20 @@ package com.company;
 
 public class SuperDemo {
     public static void main(String[] args){
-        A a=new A();
-        System.out.println();
-        B b=new B();
-        System.out.println();
+        A a=new A(); System.out.println();
+
+        B b=new B(); System.out.println();
+
         B b1=new B(2);   // output -> in A  in B - int -> it calls the super class default constr and then follows up with the sub class constr
         System.out.println();
-        B b2=new B(2,3);
+
+        B b2=new B(2,3); System.out.println(); System.out.println();
+
+        b.show();
     }
 }
 class A{
+    int i=5;
     A(){
         System.out.print("in A  ");
     }
@@ -21,8 +25,12 @@ class A{
     A(int i,int j){
         System.out.print("in A-int,int  ");
     }
+    void show(){
+        System.out.println("Show-A");
+    }
 }
 class B extends A{   //class B inherits class A
+    int i=10;
     B(){
         super(); // even not added, super() is called implicitly as explained in the b1 obj ref.
         System.out.print("in B  ");
@@ -33,5 +41,11 @@ class B extends A{   //class B inherits class A
     B(int i,int j){
         super(5,6);  //to make it call another super class constr rather than the default one
         System.out.print("in B-int,int  ");
+    }
+    @Override
+    void show(){
+        super.show(); // use of super in methods
+        System.out.println("Show-B");
+        System.out.println("Value of i : "+super.i); // use of super in variables
     }
 }
